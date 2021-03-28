d7e203fb56d8265423a2f59656d18097b0273bcc
Silviu Obreajan <obreajansilviu@yahoo.com>
2021-03-28 21:39:22 +0300

    Update Tara.java

--- src/main/java/com/example/menu/item/Tara.java
+++ src/main/java/com/example/menu/item/Tara.java
@@ -1,15 +1,25 @@
 package com.example.menu.item;
 import org.springframework.data.annotation.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 public class Tara {
 
     private final long id;
+
+    @NotNull(message = "Denumirea tarii este necesara!")
+    @Pattern(regexp="^[a-zA-Z ]+$", message = "Tara trebuie sa fie text!")
     private final String denumire;
+
+    @NotNull(message = "Denumirea regiunii este necesara!")
+    @Pattern(regexp="^[a-zA-Z ]+$", message = "Regiune trebuie sa fie text!")
     private final String regiune;
+
+    @NotNull(message = "Denumirea localitatii este necesara!")
+    @Pattern(regexp="^[a-zA-Z ]+$", message = "Localitatea trebuie sa fie text!")
     private final String localitate;
 
     public Tara(long id, String denumire, String regiune, String localitate) {
-        
         this.id = id;
         this.denumire = denumire;
         this.regiune = regiune;
