7b5862783f0c5c7def009dd8354dd0fd765d6a80
Silviu Obreajan <obreajansilviu@yahoo.com>
2021-04-01 10:19:12 +0300

    Update InMemoryTaraRepository.java

--- src/main/java/com/example/menu/item/InMemoryTaraRepository.java
+++ src/main/java/com/example/menu/item/InMemoryTaraRepository.java
@@ -2,7 +2,11 @@ package com.example.menu.item;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InMemoryTaraRepository extends CrudRepository<Tara, Long> {
 
+        List<Tara> findTaraByDenumire(String denumire);
+        List<Tara> findAll();
 }
